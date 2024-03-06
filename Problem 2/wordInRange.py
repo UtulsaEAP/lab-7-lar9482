51bb615604cca86f334b685fc818946b7ea79762
lar9482 <lar9482@utulsa.edu>
2024-03-06 14:30:38 -0600

    Completed problem 2

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -5,6 +5,35 @@
 
 def wordInRange():
     #Type your code here
-    return
+    inputFileName = input()
+    firstWord = input()
+    secondWord = input()
+
+    with open(inputFileName, "r") as file:
+        fileWords = file.readlines()
+
+        for word in fileWords:
+            word = word.strip("\n")
+
+            if (isInclusive(word, firstWord, secondWord)):
+                print(f'{word} - in range')
+            else:
+                print(f'{word} - not in range')
+
+def isInclusive(word, firstWord, secondWord):
+    for i in range(0, len(word)):
+        if (ord(word[i]) < ord(firstWord[i])):
+            return False
+        if (ord(word[i]) > ord(firstWord[i])):
+            break
+    
+    for i in range(0, len(word)):
+        if (ord(word[i]) > ord(secondWord[i])):
+            return False
+        if (ord(word[i]) < ord(secondWord[i])):
+            break
+
+    return True
+
 if __name__ == '__main__':
     wordInRange()
