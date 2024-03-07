227e70793525b8b14ff6bd01af17bd931504b495
lar9482 <lar9482@utulsa.edu>
2024-03-06 19:11:57 -0600

    Completed problem 4

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -5,7 +5,14 @@
 
 def fileNameChange():
     #type your code here
-
+    filepath = input()
+    
+    with open(filepath, "r") as file:
+        photoFileNames = file.readlines()
+        for photoFileName in photoFileNames:
+            photoFileName_Parts = photoFileName.split('_')
+            photoName = photoFileName_Parts[0]
+            print(f'{photoName}_info.txt')
     return
 
 if __name__ == '__main__':
